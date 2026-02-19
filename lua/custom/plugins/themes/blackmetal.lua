ad11a3033105a3792004f9f29725b5574d9174c7
shaunakpandit <shaunakdpandit@gmail.com>
2026-02-19 00:47:04 -0500

    try again
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- lua/custom/plugins/themes/blackmetal.lua
+++ lua/custom/plugins/themes/blackmetal.lua
@@ -4,7 +4,7 @@ return {
   priority = 1,
   config = function()
     require('black-metal').setup {
-      theme = 'Venom',
+      theme = 'venom',
       -- Can be one of: 'light' | 'dark', or set via vim.o.background
       variant = 'dark',
     }
