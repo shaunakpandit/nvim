9972e69652073e66b4fbd4006ec0c92738673af4
shaunakpandit <shaunakdpandit@gmail.com>
2026-02-19 00:40:12 -0500

    test this out
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- lua/custom/plugins/themes/blackmetal.lua
+++ lua/custom/plugins/themes/blackmetal.lua
@@ -4,7 +4,7 @@ return {
   priority = 1,
   config = function()
     require('black-metal').setup {
-      theme = 'immortal',
+      theme = 'Venom',
       -- Can be one of: 'light' | 'dark', or set via vim.o.background
       variant = 'dark',
     }
